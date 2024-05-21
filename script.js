d14eafad5c87274fad50abdb8df8df6efa430fee
Sand <sandboxasdf@gmail.com>
2024-05-21 18:27:17 +1000

    dark mode in progress

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ function RecolorCalculator() {
     });
     */
 }
+
+// dark mode
+
+const darkModeSwitch = document.querySelector("#darkModeToggle");
+darkModeSwitch.addEventListener("click", ToggleDarkMode)
+
+function ToggleDarkMode() {
+    let gridContainer = document.querySelector(".gridContainer");
+    gridContainer.classList.toggle("gridContainerDark");
+
+    document.querySelectorAll(".bottomSectionProjectsPanel").forEach(function(panel) {
+        panel.classList.toggle("bottomSectionProjectsPanelDark");
+    });
+
+    let bottom = document.querySelector(".bottomSection");
+    bottom.classList.toggle("bottomSectionDark");
+
+    document.querySelectorAll(".bottomSectionAnnouncementsBubble").forEach(function(bubble) {
+        bubble.classList.toggle("bottomSectionAnnouncementsBubbleDark");
+    });
+
+    /*
+    let calculator = document.querySelector(".calcSection");
+    let content = document.querySelector(".content");
+    let buttonSection = document.querySelector(".buttonsSection");
+    let display = document.querySelector(".displayRow");
+
+    calculator.classList.toggle("calcSectionDark");
+    content.classList.toggle("contentDark");
+    buttonSection.classList.toggle("buttonDark");
+    display.classList.toggle("displayRowDark");
+    */
+}
